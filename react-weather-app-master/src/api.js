ede803313b96a0ac3f5a1d5431df11d1a2a29e94
sakshigujar26 <2022bit@sggs.ac.in>
2024-09-05 10:43:22 +0530

    Weather app

---
+++ react-weather-app-master/src/api.js
@@ -0,0 +1,14 @@
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '63a5852c8bmsh841a04b7a08bc1ep168ee9jsncd3f935d2a90',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL= "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "854616101e50c1bc8cc4cd5179c14535"
